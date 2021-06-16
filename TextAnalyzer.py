96db5e7c83714ab2ebd19f20f8051098ac0d2531
Petr Hornik <petr.hornik@gmail.com>
2021-06-16 15:17:23 +0200

    project revision 2

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -1,8 +1,6 @@
 from task_template import TEXTS
 
 user_data = dict(bob='123', ann='pass123', mike='password123', liz='pass123')
-n_words = n_titlecase = n_upercase = n_lowercase = n_numers = sum_numbers = n_rest = 0
-n_len = {}
 separator = "-" * 40
 
 # user verification
@@ -16,12 +14,18 @@ else:
         print(f"Invalid password. Exiting program.")
     else:
         # say hello
-        print(f"Welcome to the app, {username}\n We have 3 texts to be analyzed.")
+        n_texts = len(TEXTS)
+        print(f"Welcome to the app, {username}\n We have {n_texts} texts to be analyzed.")
         print(separator)
 
         # text selection
-        n_texts = len(TEXTS)
-        text_number = int(input(f"Enter a number btw. 1 and {n_texts} to select: "))
+        while True:
+            try:
+                text_number = int(input(f"Enter a number btw. 1 and {n_texts} to select: "))
+                break
+            except ValueError:
+                print("Input is not numeric.")
+
         if text_number - 1 not in range(n_texts):
             print("Invalid text number. Exiting program.")
         else:
@@ -29,31 +33,30 @@ else:
 
             # text evaluation
             text = TEXTS[text_number - 1].split(" ")
-            cleaned_text = []
-            for word in text:
-                cleaned_text.append(word.strip(".,:?!\n\r "))
-            if "" in cleaned_text:
-                cleaned_text.remove("")
+            cleaned_text = [word.strip(".,:?!\n\r ") for word in text]
+            cleaned_text = [item for item in cleaned_text if item != '']
 
+            n_len = {}
             n_words = len(cleaned_text)
+            n_titlecase = n_uppercase = n_lowercase = n_numbers = sum_numbers = n_rest = 0
             for word in cleaned_text:
                 if word.isalpha():
                     n_titlecase += 1 if word.istitle() or word.isupper() else 0
-                    n_upercase += 1 if word.isupper() else 0
+                    n_uppercase += 1 if word.isupper() else 0
                     n_lowercase += 1 if word.islower() else 0
                 elif word.isnumeric():
-                    n_numers += 1
+                    n_numbers += 1
                     sum_numbers += int(word)
                 else:
-                    n_rest += 1;
+                    n_rest += 1
                 word_length = len(word)
                 n_len[word_length] = n_len.setdefault(word_length, 0) + 1
 
             print(f"There are {n_words} words in the selected text.")
             print(f"There are {n_titlecase} titlecase words.")
-            print(f"There are {n_upercase} uppercase words.")
+            print(f"There are {n_uppercase} uppercase words.")
             print(f"There are {n_lowercase} lowercase words.")
-            print(f"There are {n_numers} numeric strings.")
+            print(f"There are {n_numbers} numeric strings.")
             print(f"The sum of all the numbers {sum_numbers}")
 
             # word length graph plotting
